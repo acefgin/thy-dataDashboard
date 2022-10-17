6ddf1a08fb41bbdd33620f26ac43cbc76f1417b8
cifeng1989 <ray.cifeng@gmail.com>
2022-10-17 11:46:28 -0700

    testInfo update, edit, dump2Json

--- resources/customWidgets.py
+++ resources/customWidgets.py
@@ -6,6 +6,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QDir
 from PyQt5.QtWidgets import *
 
+import os, csv, glob
+import json
+
+from pathlib import Path
+
 class dataVisual_Widget(QtWidgets.QWidget, dataVisualWidget_Form):
     def __init__(self, parent=None):
         super(dataVisual_Widget, self).__init__(parent)
@@ -49,10 +54,40 @@ class testImport_Widget(QtWidgets.QWidget, testImportWidget_Form):
     def __init__(self, parent=None):
         super(testImport_Widget, self).__init__(parent)
         self.setupUi(self)
+        self.objectDis = {}
+        self.filenamesSet = set()
+
         self.pushButton.clicked.connect(self.createTest)
         self.pushButton_2.clicked.connect(self.deleteTest)
         self.pushButton_7.clicked.connect(self.selectFolder)
-        self.objectDis = {}
+        self.pushButton_5.clicked.connect(self.modifyTest)
+        self.pushButton_6.clicked.connect(self.checkMap)
+        self.pushButton_4.clicked.connect(self.dumpTestInfo2JsonFile)
+
+        # creating editable combo box
+        self.comboBox.setEditable(True)
+        self.comboBox_2.setEditable(True)
+        self.comboBox_3.setEditable(True)
+        self.comboBox_4.setEditable(True)
+        self.comboBox_5.setEditable(True)
+  
+        # setting insertion policy
+        # new item will get added alphabetically
+        self.comboBox.setInsertPolicy(QComboBox.InsertAlphabetically)
+        self.comboBox_2.setInsertPolicy(QComboBox.InsertAlphabetically)
+        self.comboBox_3.setInsertPolicy(QComboBox.InsertAlphabetically)
+        self.comboBox_4.setInsertPolicy(QComboBox.InsertAlphabetically)
+        self.comboBox_5.setInsertPolicy(QComboBox.InsertAlphabetically)
+
+        self.listWidget.itemDoubleClicked.connect(self.select2Edit)
+
+
+        self.checkTestInfoMsg = QMessageBox()
+        self.checkTestInfoMsg.setWindowTitle("Please check the new tests")
+        self.checkTestInfoMsg.setText("Raw file not found for some tests")
+        self.checkTestInfoMsg.setIcon(QMessageBox.Warning)
+
+        self.mapChkPassed = False
 
     def createTest(self):
         date = self.lineEdit.text()
@@ -64,6 +99,9 @@ class testImport_Widget(QtWidgets.QWidget, testImportWidget_Form):
         protocol = self.comboBox.currentText()
         devOperator = self.comboBox_2.currentText()
         kitOperator = self.comboBox_3.currentText()
+
+        usage = self.comboBox_4.currentText()
+        asExpected = self.comboBox_5.currentText()
 
         testId = date + '_' + deviceId + f'_{runNum}'
 
@@ -77,6 +115,8 @@ class testImport_Widget(QtWidgets.QWidget, testImportWidget_Form):
         document["Input"] = input
         document["TestDate"] = date
         document["DeviceId"] = deviceId
+        document["Usage"] = usage
+        document["AsExpected"] = asExpected
 
         self.listWidget.addItem(f'{testId}==>{protocol}==>{input}@{location}')
         self.objectDis[testId] = document
@@ -88,22 +128,241 @@ class testImport_Widget(QtWidgets.QWidget, testImportWidget_Form):
         self.listWidget.takeItem(curRow)
         testId = curText[:curText.index("=")]
         self.objectDis.pop(testId)
+    
+    def modifyTest(self):
+        date = self.lineEdit.text()
+        deviceId = self.lineEdit_2.text()
+        runNum = self.spinBox.value()
+        location = self.lineEdit_4.text()
+        input = self.lineEdit_3.text()
+        kitLot = self.lineEdit_5.text()
+        protocol = self.comboBox.currentText()
+        devOperator = self.comboBox_2.currentText()
+        kitOperator = self.comboBox_3.currentText()
+
+        usage = self.comboBox_4.currentText()
+        asExpected = self.comboBox_5.currentText()
+
+        testId = date + '_' + deviceId + f'_{runNum}'
+
+        document = self.objectDis[testId]
+
+        document["TestId"] = testId
+        document["Protocol"] = protocol
+        document["Location"] = location
+        document["DeviceOperator"] = devOperator
+        document["devOperator"] = kitOperator
+        document["KitLot"] = kitLot
+        document["Input"] = input
+        document["TestDate"] = date
+        document["DeviceId"] = deviceId
+        document["Usage"] = usage
+        document["AsExpected"] = asExpected
+
+        curRow = self.listWidget.currentRow()
+        self.listWidget.takeItem(curRow)
+        self.listWidget.addItem(f'{testId}==>{protocol}==>{input}@{location}')
+
+    
+    def select2Edit(self, listWidgeItem):
+        curText = listWidgeItem.text()
+        testId = curText[:curText.index("=")]
+        document = self.objectDis[testId]
+        self.updateInputFields(document)
+    
+    def updateInputFields(self, document):
+        testId = document["TestId"]
+        protocol = document["Protocol"] 
+        runNum = int(testId[-1])
+        location = document["Location"]
+        devOperator= document["DeviceOperator"]
+        kitOperator = document["devOperator"]
+        kitLot = document["KitLot"]
+        input = document["Input"]
+        date = document["TestDate"]
+        deviceId = document["DeviceId"]
+        usage = document["Usage"]
+        asExpected = document["AsExpected"]
+
+        self.lineEdit.setText(date)
+        self.lineEdit_2.setText(deviceId)
+        self.spinBox.setValue(runNum)
+        self.lineEdit_4.setText(location)
+        self.lineEdit_3.setText(input)
+        self.lineEdit_5.setText(kitLot)
+
+        self.updateComboBox(self.comboBox, protocol)
+        self.updateComboBox(self.comboBox_2, devOperator)
+        self.updateComboBox(self.comboBox_3, kitOperator)
+        self.updateComboBox(self.comboBox_4, usage)
+        self.updateComboBox(self.comboBox_5, asExpected)
+
+    
+    def updateComboBox(self, comboBox, itemText):
+        for i in range(comboBox.count()):
+            if itemText == comboBox.itemText(i):
+                comboBox.setCurrentIndex(i)
 
     def selectFolder(self):
-        folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
+        self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
         model = QFileSystemModel()
-        model.setRootPath(folderpath)
+        model.setRootPath(self.folderpath)
         
         self.listView.setModel(model)
-        self.listView.setRootIndex(model.index(folderpath))
+        self.listView.setRootIndex(model.index(self.folderpath))
+
+        self.filepathes = sorted(glob.glob(os.path.join(self.folderpath, '*.csv')))
+        
+        for filepath in self.filepathes:
+            self.filenamesSet.add(Path(filepath).stem)
+    
+    def checkMap(self):
+        cnt = 0
+        for idx in range(0, self.listWidget.count()):
+            item = self.listWidget.item(idx)
+            itemText = item.text()
+            testId = itemText[:itemText.index("=")]
+
+            if testId in self.filenamesSet:
+                item.setForeground(QtCore.Qt.green)
+                cnt += 1
+            else:
+                item.setForeground(QtCore.Qt.red)
+        if cnt != 0 and cnt == self.listWidget.count():
+            self.mapChkPassed = True
+
+    def dumpTestInfo2JsonFile(self):
+
+        filenames = self.filepathes
+        jsonFile = "S2R_testlog.json"
+        if not self.mapChkPassed:
+            msg = self.checkTestInfoMsg.exec_()
+            return
+
+        with open(jsonFile,'w') as file:
+
+            idx = 1
+            objList = []
+            for filename in filenames:
+                testId = os.path.splitext(os.path.basename(filename))[0]
+                
+                idx += 1
+                if testId in self.objectDis:
+                    testDocument = self.objectDis[testId]
+                    resultDocument = self.readTestRsult(self.folderpath, filename)
+                    self.Merge(resultDocument, testDocument)
+                    objList.append(testDocument)
+                    print(str(idx) + ":" + testId)
+                
+            json.dump(objList, file, indent = 2)
+    
+    # Merge dict1 to dict2, update in dict2
+    def Merge(self, dict1, dict2):
+        return(dict2.update(dict1))
+    
+    def readTestRsult(self, folderPath, filename):
+
+        LysisTemp = []
+        LysisTime = []
+        CartTemp = []
+        AdcTemp = []
+        TargetName = []
+        WellResult = []
+        ECVolDiffs = []
+        SampleId = ''
+
+        ReactTime = []
+        Well1Readings = []
+        Well2Readings = []
+        Well3Readings = []
+        Well4Readings = []
+        Well5Readings = []
+
+        dcoument = {}
+
+        with open(os.path.join(folderPath, filename),'r') as csvfile:
+            plots = csv.reader(csvfile, delimiter=',')
+            idx = 0
+
+            for row in plots:
+                if idx == 1:
+                    SampleId = row[0]
+                if idx == 4:
+                    LysisTime = row[3:]
+                    LysisTime = [round(float(i)/1000, 2) for i in LysisTime if i != '']
+                if idx == 5:
+                    LysisTemp = row[3:]
+                    LysisTemp = [round(float(i), 2) for i in LysisTemp if i != '']
+                if idx == 8:
+                    ReactTime = row[7:]
+                    ReactTime = [round(float(i)/1000, 2) for i in ReactTime if i != '']
+                if idx == 9:
+                    CartTemp = row[7:]
+                    CartTemp = [round(float(i), 2) for i in CartTemp if i != '']
+                if idx == 10:
+                    AdcTemp = row[7:]
+                    AdcTemp = [round(float(i), 2) for i in AdcTemp if i != '']
+                if idx == 11:
+
+                    TargetName.append(row[4])
+                    WellResult.append(row[5])
+                    ECVolDiffs.append(round(float(row[6]), 2))
+                    Well1Readings = row[7:]
+                    Well1Readings = [round(float(i), 2) for i in Well1Readings if i != '']
+
+                if idx == 12:
+
+                    TargetName.append(row[4])
+                    WellResult.append(row[5])
+                    ECVolDiffs.append(round(float(row[6]), 2))
+                    Well2Readings = row[7:]   
+                    Well2Readings = [round(float(i), 2) for i in Well2Readings if i != '']
+
+                if idx == 13:
+
+                    TargetName.append(row[4])
+                    WellResult.append(row[5])
+                    ECVolDiffs.append(round(float(row[6]), 2))
+                    Well3Readings = row[7:]
+                    Well3Readings = [round(float(i), 2) for i in Well3Readings if i != '']
+
+                if idx == 14:
+
+                    TargetName.append(row[4])
+                    WellResult.append(row[5])
+                    ECVolDiffs.append(round(float(row[6]), 2))
+                    Well4Readings = row[7:]
+                    Well4Readings = [round(float(i), 2) for i in Well4Readings if i != '']
+
+                if idx == 15:
+
+                    TargetName.append(row[4])
+                    WellResult.append(row[5])
+                    ECVolDiffs.append(round(float(row[6]), 2))
+                    Well5Readings = row[7:]
+                    Well5Readings = [round(float(i), 2) for i in Well5Readings if i != '']
+                
+                idx += 1
+
+        dcoument["LysisTemp"] = LysisTemp
+        dcoument["LysisTime"] = LysisTime
+        dcoument["CartTemp"] = CartTemp
+        dcoument["AdcTemp"] = AdcTemp
+        dcoument["TargetName"] = TargetName
+        dcoument["WellResult"] = WellResult
+        dcoument["ECVolDiffs"] = ECVolDiffs
+        dcoument["SampleId"] = SampleId
+
+        dcoument["ReactTime"] = ReactTime
+        dcoument["Well1Readings"] = Well1Readings
+        dcoument["Well2Readings"] = Well2Readings
+        dcoument["Well3Readings"] = Well3Readings
+        dcoument["Well4Readings"] = Well4Readings
+        dcoument["Well5Readings"] = Well5Readings
+
+        return dcoument
         
 
-
-
-
-        
-
-        
 
 if __name__ == "__main__":
     import sys
